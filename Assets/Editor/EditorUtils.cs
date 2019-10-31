be452420bd2812c189639ce82a7072676ac650d3
Petros Kitsaras <petros.kitsaras@gmail.com>
2019-10-31 01:22:34 +0100

    deleting assets not needed + recreating all prefabs + deleting all .meta to reimport them

---
+++ Assets/Editor/EditorUtils.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public static class EditorUtils
+{
+    [MenuItem("Utils/Reserialize all prefabs")]
+    private static void onClick_ReserializeAllPrefabs()
+    {
+        foreach (string _prefabPath in GetAllPrefabs())
+        {
+            GameObject _prefabAsset = AssetDatabase.LoadAssetAtPath<GameObject>(_prefabPath);
+            if (!PrefabUtility.IsPartOfImmutablePrefab(_prefabAsset))
+            {
+                PrefabUtility.SavePrefabAsset(_prefabAsset);
+            }
+        }
+    }
+
+    public static string[] GetAllPrefabs()
+    {
+        List<string> _prefabPaths = new List<string>();
+        foreach (string _paths in AssetDatabase.GetAllAssetPaths())
+        {
+            if (_paths.Contains(".prefab"))
+            {
+                _prefabPaths.Add(_paths);
+            }
+        }
+        return _prefabPaths.ToArray();
+    }
+}
