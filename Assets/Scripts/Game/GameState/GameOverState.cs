21b9e2f06136df3d35c87832c6e57b1d7773d75e
Petros Kitsaras <petros.kitsaras@gmail.com>
2019-10-31 19:01:25 +0100

    clean project unused assets

---
+++ Assets/Scripts/Game/GameState/GameOverState.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class GameOverState : State
+{
+    public GameOverState(GameManager gameManager) : base(gameManager)
+    {
+    }
+
+    public override void OnStateEnter()
+    {
+        base.OnStateEnter();
+
+        SpawnEnemies.GameOver = true;
+        Player player = PlayerManager.GetPlayer();
+        PlayerAnimation playerAnimation = player.PlayerAnimation();
+
+        if (player != null)
+        {
+            AudioManager.PlaySound("Victory", 3);
+            playerAnimation.Exit();
+        }
+        else if (player == null)
+        {
+            AudioManager.PlaySound("GameOver", 3);
+        }
+
+
+ 
+    }
+
+    public override void OnStateExit()
+    {
+        base.OnStateExit();
+    }
+
+    public override void Tick()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Game/GameState/MenuState.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuState : State
+{
+    public MenuState(GameManager gameManager) : base(gameManager)
+    {
+    }
+    public override void OnStateEnter() {
+        Application.targetFrameRate = 30;
+    }
+    public override void OnStateExit() { }
+
+    public override void Tick()
+    {
+      
+    }
+}
