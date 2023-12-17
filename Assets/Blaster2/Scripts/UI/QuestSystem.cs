82a04d5c2735786a6d7c04b9ce08736846b1c61d
Petros Kitsaras <13155066+TheGamerUrso@users.noreply.github.com>
2023-12-17 14:50:53 +0100

    fixed survival gameover and win screen and pause screen

--- Assets/Blaster2/Scripts/UI/QuestSystem.cs
+++ Assets/Blaster2/Scripts/UI/QuestSystem.cs
@@ -199,7 +199,8 @@ public class QuestSystem : MonoSingleton<QuestSystem>
 
     public void PlayButton()
     {       
-        LevelEnum[] levels ={LevelEnum.Level0,LevelEnum.Level1,LevelEnum.Level2,LevelEnum.Level3,LevelEnum.Level4,LevelEnum.Level5,LevelEnum.Level6,LevelEnum.Level7,LevelEnum.Level8,LevelEnum.Level9};
+       // LevelEnum[] levels ={LevelEnum.Level0,LevelEnum.Level1,LevelEnum.Level2,LevelEnum.Level3,LevelEnum.Level4,LevelEnum.Level5,LevelEnum.Level6,LevelEnum.Level7,LevelEnum.Level8,LevelEnum.Level9};
+        LevelEnum[] levels ={LevelEnum.Level0};
         GameManager.Instance.LoadScene(levels[UnityEngine.Random.Range(0,levels.Length)]);
     }
 }
